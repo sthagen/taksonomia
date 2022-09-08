61aa5598369f0a6ec3b0a73c702dfac27b4478d7
Stefan Hagen <stefan@hagen.link>
2022-09-08 23:12:40 +0200

    Added hash avalanches based on hexdigest hashing

--- taksonomia/taksonomia.py
+++ taksonomia/taksonomia.py
@@ -8,6 +8,7 @@ EMPTY_SHA512 = (
     'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce'
     '47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 )
+ENCODING = 'utf-8'
 TS_FORMAT = '%Y-%m-%d %H:%M:%S.%f +00:00'
 
 
@@ -18,18 +19,20 @@ class Taxonomy:
         """Construct a collector instance for root."""
         self.root = root
         self.tree = {'sha512': EMPTY_SHA512, 'count_folders': 0, 'count_files': 0, 'branches': {}, 'leaves': {}}
+        self.shadow = {'sha512_hash': hashlib.sha512(), 'branches': {}}
 
     def branch(self, path: pathlib.Path) -> None:
         """Add a folder (sub tree) entry."""
         st = path.stat()
-
-        self.tree['branches'][str(path)] = {  # type: ignore
+        branch = str(path)
+        self.tree['branches'][branch] = {  # type: ignore
             'sha512': EMPTY_SHA512,
             'count_folders': 1,
             'count_files': 0,
             'size_bytes': 0,
             'mod_time': dti.datetime.fromtimestamp(st.st_ctime, tz=dti.timezone.utc).strftime(TS_FORMAT),
         }
+        self.shadow['branches'][branch] = hashlib.sha512()  # type: ignore
         for parent in path.parents:
             branch = str(parent)
             if branch in self.tree['branches']:  # type: ignore
@@ -57,11 +60,15 @@ class Taxonomy:
             'mod_time': dti.datetime.fromtimestamp(st.st_ctime, tz=dti.timezone.utc).strftime(TS_FORMAT),
         }
 
+        self.shadow['sha512_hash'].update(hash.encode(ENCODING))  # type: ignore
+        self.tree['sha512'] = self.shadow['sha512_hash'].hexdigest()  # type: ignore
         for parent in path.parents:
             branch = str(parent)
             if branch in self.tree['branches']:  # type: ignore
                 self.tree['branches'][branch]['count_files'] += 1  # type: ignore
                 self.tree['branches'][branch]['size_bytes'] += st.st_size  # type: ignore
+                self.shadow['branches'][branch].update(hash.encode(ENCODING))  # type: ignore
+                self.tree['branches'][branch]['sha512'] = self.shadow['branches'][branch].hexdigest()  # type: ignore
 
     def __repr__(self) -> str:
         """Express yourself."""
@@ -80,5 +87,4 @@ def main(root: pathlib.Path) -> Taxonomy:
     for path in sorted(root.rglob('*')):
         if path.is_file():
             taxonomy.leaf(path)
-    print(taxonomy)
     return taxonomy
